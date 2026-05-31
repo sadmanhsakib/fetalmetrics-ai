0edef6ee7d08ded55485caa1cb8ed39a80f91400
Sadman H. Sakib <831.sakib@gmail.com>
2026-06-01 00:02:36 +0600

    added uv files, main.py and basic files

---
+++ scripts/main.py
@@ -0,0 +1,10 @@
+import time
+
+def main():
+    print("Hello from fetalmetrics-ai!")
+
+
+if __name__ == "__main__":
+    start_time = time.time()
+    main()
+    print(f"✅ Execution completed in {time.time() - start_time:.2f} seconds")
