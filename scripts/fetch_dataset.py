79bbb34a49df2fa4b110c863b415a5bcecd6971d
sadmanhsakib <831.sakib@gmail.com>
2026-07-06 08:55:11 +0600

    feat: now directly moved in data/raw/

--- scripts/fetch_dataset.py
+++ scripts/fetch_dataset.py
@@ -1,12 +1,15 @@
 import os
+import shutil
 
 import kagglehub
+from pyprojroot import here
 
 download_path = "data/raw/"
 os.makedirs(download_path, exist_ok=True)
 
 # Download the dataset
-path = kagglehub.dataset_download("thanhbnhphan/hc18-grand-challenge",
-                                  path=download_path)
+path = kagglehub.dataset_download("thanhbnhphan/hc18-grand-challenge")
+# move the dataset
+shutil.move(path, here(download_path))
 
 print("Path to dataset files:", path)
