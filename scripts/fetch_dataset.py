d0fb588f089d63246f9a1f1275cdb58b894a5c6b
sadmanhsakib <831.sakib@gmail.com>
2026-07-06 18:30:20 +0600

    fix: bug fixes and refactoring

--- scripts/fetch_dataset.py
+++ scripts/fetch_dataset.py
@@ -5,19 +5,24 @@ import kagglehub
 
 destination_path = "data/"
 
-# removing the old dir for recency
-if os.path.exists(destination_path):
-    shutil.rmtree(destination_path)
 
-os.makedirs(destination_path, exist_ok=True)
+def fetch_dataset():
+    # removing the old dir for recency
+    if os.path.exists(destination_path):
+        shutil.rmtree(destination_path)
 
-# Download the dataset
-path = kagglehub.dataset_download("thanhbnhphan/hc18-grand-challenge")
+    os.makedirs(destination_path, exist_ok=True)
 
-# move the dataset
-shutil.move(path, destination_path)
+    # Download the dataset
+    path = kagglehub.dataset_download("thanhbnhphan/hc18-grand-challenge")
 
-os.rename(os.path.join(destination_path, os.path.basename(path)),
-          os.path.join(destination_path, "raw"))
+    # move the dataset
+    shutil.move(path, destination_path)
 
-print(f"✅ Dataset stored in {destination_path}/raw")
+    os.rename(os.path.join(destination_path, os.path.basename(path)),
+            os.path.join(destination_path, "raw"))
+
+    print(f"✅ Dataset stored in {destination_path}/raw")
+
+if __name__ == "__main__":
+    fetch_dataset()
